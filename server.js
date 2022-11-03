53506a5dbad26630c7303665565d20ae5312463f
Alex Covone <alexcovone@gmail.com>
2022-11-03 16:33:29 -0400

    Shopping list

--- server.js
+++ server.js
@@ -23,8 +23,13 @@ app.use(express.json())
 
 app.get('/',async (request, response)=>{
     const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    const itemsPurchased = await db.collection('todos').countDocuments({completed: true})
+    const itemsInCart = await db.collection('todos').countDocuments({completed: false})
+    response.render('index.ejs',{ 
+        items: todoItems, inCart: itemsInCart, purchased: itemsPurchased
+    })
+    // response.render('index.ejs', { items: todoItems, purchased: itemsPurchased })
+    
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -36,13 +41,22 @@ app.get('/',async (request, response)=>{
 })
 
 app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false, itemCost: request.body.itemCost})
     .then(result => {
-        console.log('Todo Added')
+        console.log('Item Added to Cart')
         response.redirect('/')
     })
     .catch(error => console.error(error))
 })
+
+// app.post('/budget', (request, response) => {
+//     db.collection('todos').insertOne({moneyLeft: request.body.budget})
+//     .then(result => {
+//         console.log('This is the remaining budget.')
+//         response.redirect('/')
+//     })
+//     .catch(error => console.error(error))
+// })
 
 app.put('/markComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
@@ -54,8 +68,8 @@ app.put('/markComplete', (request, response) => {
         upsert: false
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Item Purchased')
+        response.json('Item Purchased')
     })
     .catch(error => console.error(error))
 
@@ -71,8 +85,8 @@ app.put('/markUnComplete', (request, response) => {
         upsert: false
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Item Not Purchased')
+        response.json('Item Not Purchased')
     })
     .catch(error => console.error(error))
 
@@ -81,8 +95,8 @@ app.put('/markUnComplete', (request, response) => {
 app.delete('/deleteItem', (request, response) => {
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
+        console.log('Item Removed from Cart')
+        response.json('Item Removed from Cart')
     })
     .catch(error => console.error(error))
 
